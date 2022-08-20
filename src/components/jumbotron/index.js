8ba615d897a76095cbd3edb905872a371f4b7132
Gáll Botond <gallbotondofficial@gmail.com>
2022-08-20 18:39:41 +0300

    Compound components design implementatin begin

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,25 @@
+import { Item, Inner, Container, Title, SubTitle } from "./styles/jumbotron";
+
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  );
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubtitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Jumbotron from "../components/jumbotron";
+
+export default function Home() {
+  return (
+  <Jumbotron.Container>
+    <Jumbotron.Title>Hello</Jumbotron.Title>
+    <Jumbotron.SubTitle>Hello</Jumbotron.SubTitle>
+  </Jumbotron.Container>);
+}
