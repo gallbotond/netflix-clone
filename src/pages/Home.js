9357a12253551cf488a7ade32fe70a6f29ed957d
Gáll Botond <gallbotondofficial@gmail.com>
2022-08-25 12:18:03 +0300

    started implementing Footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
 import JumbotronContainer from "../containers/JumbotronContainer";
+import FooterContainer from "../containers/FooterContainer";
 
 export default function Home() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 }
