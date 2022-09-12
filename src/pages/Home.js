60aa9f415e028eb16ac7e60730f44b27aad83986
Gáll Botond <gallbotondofficial@gmail.com>
2022-09-12 22:45:42 +0300

    finished header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,21 @@ import JumbotronContainer from "../containers/JumbotronContainer";
 import FooterContainer from "../containers/FooterContainer";
 import FAQsContainer from "../containers/FAQsContainer";
 import HeaderContainer from "../containers/HeaderContainer";
+import OptForm from "../components/opt-form";
 
 export default function Home() {
   return (
     <>
       <HeaderContainer>
-        
+        <OptForm>
+          <OptForm.Input placeholder="Email Address" />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </OptForm.Text>
+        </OptForm>
       </HeaderContainer>
       <JumbotronContainer />
       <FAQsContainer />
