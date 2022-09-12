9a8667eec4b42bba0295987b3d91d83db10c5d96
Gáll Botond <gallbotondofficial@gmail.com>
2022-09-12 22:20:00 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import * as ROUTES from "./constants/routes";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home></Home>}></Route>
-      <Route path="/signin" element={<h1>signin</h1>}></Route>
-      <Route path="/signup" element={<h1>signup</h1>}></Route>
-      <Route path="/browse" element={<h1>browse</h1>}></Route>
+      <Route path={ROUTES.HOME} element={<Home></Home>}></Route>
+      <Route path={ROUTES.SIGN_IN} element={<h1>signin</h1>}></Route>
+      <Route path={ROUTES.SIGN_UP} element={<h1>signup</h1>}></Route>
+      <Route path={ROUTES.BROWSE} element={<h1>browse</h1>}></Route>
     </Routes>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,14 @@ import React from "react";
 import JumbotronContainer from "../containers/JumbotronContainer";
 import FooterContainer from "../containers/FooterContainer";
 import FAQsContainer from "../containers/FAQsContainer";
+import HeaderContainer from "../containers/HeaderContainer";
 
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        
+      </HeaderContainer>
       <JumbotronContainer />
       <FAQsContainer />
       <FooterContainer />
