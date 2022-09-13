26c5c9a964c4efd54d3714c41d4191965c51c753
Gáll Botond <gallbotondofficial@gmail.com>
2022-09-13 19:09:39 +0300

    front page fully finished

---
+++ src/containers/FeatureContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Feature from "../components/feature";
+
+export default function FeatureContainer() {
+  return (
+    <Feature.Container>
+      <Feature.Title>This is a feature title</Feature.Title>
+      <Feature.Subtitle>This is a subtitle of a feature</Feature.Subtitle>
+    </Feature.Container>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,20 +4,27 @@ import FooterContainer from "../containers/FooterContainer";
 import FAQsContainer from "../containers/FAQsContainer";
 import HeaderContainer from "../containers/HeaderContainer";
 import OptForm from "../components/opt-form";
+import { Feature } from "../components";
 
 export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <OptForm>
-          <OptForm.Input placeholder="Email Address" />
-          <OptForm.Button>Try it now</OptForm.Button>
-          <OptForm.Break />
-          <OptForm.Text>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </OptForm.Text>
-        </OptForm>
+        <Feature>
+            <Feature.Title>
+              Unlimited films, TV programmes and more!
+            </Feature.Title>
+            <Feature.Subtitle>Watch anywhere. Cancel anytime.</Feature.Subtitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
       </HeaderContainer>
       <JumbotronContainer />
       <FAQsContainer />
