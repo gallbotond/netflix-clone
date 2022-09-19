21e32cc3ed7b2671d53218fab535ac3ffe05445e
Gáll Botond <gallbotondofficial@gmail.com>
2022-09-19 20:09:00 +0300

    styling

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -108,3 +108,23 @@ export const FeatureCallOut = styled.h2`
   text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
   margin: 0;
 `;
+
+export const PlayButton = styled.button`
+  box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
+  background-color: #e6e6e6;
+  color: #000;
+  border-width: 0;
+  padding: 10px 20px;
+  border-radius: 5px;
+  max-width: 130px;
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: 10px;
+  cursor: pointer;
+  transition: background-color 0.5s ease;
+
+  &:hover {
+    background: #ff1e1e;
+    color: white;
+  }
+`;
